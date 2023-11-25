3c7fad3797e4fd5d4aa22568f2c59fe249c9c553
zhaoyd5268 <zhaoyd5268@gmail.com>
2023-11-25 10:57:08 -0800

    Finished with Module 1(Level 5, projects all finished?)?

--- src/_04_Directory_Iteration/DirectoryIterator.java
+++ src/_04_Directory_Iteration/DirectoryIterator.java
@@ -6,28 +6,38 @@ import javax.swing.JFileChooser;
 
 public class DirectoryIterator {
 	public static void main(String[] args) {
-		/* 
+		/*
 		 * The following is an example of how to list all of the files in a directory.
 		 * Once the program is running, the directory is chosen using the JFileChooser.
 		 */
-		JFileChooser jfc = new JFileChooser();
-		jfc.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-		int returnVal = jfc.showOpenDialog(null);
-		if (returnVal == JFileChooser.APPROVE_OPTION) {
-			File directory = jfc.getSelectedFile();
-			File[] files = directory.listFiles();
-			if(files != null) {
-				for(File f : files) {
-				  System.out.println(f.getAbsolutePath());
+
+		/*
+		 * Your task is to write a program that iterates through the src folder of this
+		 * current Java Project. For every .java file it finds, the program will add a
+		 * (non-legally binding) copyright statement at the bottom. Be aware of possible
+		 * directories inside of directories. (e.g //Copyright © 2019 FirstName
+		 * LastName)
+		 */
+
+		JFileChooser jfc1 = new JFileChooser();
+		jfc1.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+		int returnVal1 = jfc1.showOpenDialog(null);
+		if (returnVal1 == JFileChooser.APPROVE_OPTION) {
+			File directory1 = jfc1.getSelectedFile();
+			System.out.println(directory1);
+			File[] files1 = directory1.listFiles();
+			for(int i = 0; i < 8; i++) {
+				File[] f = files1[i].listFiles();
+
+			if (files1 != null) {
+				for (File f1 : f) {
+					if (f1.getName().contains(".java")) {
+						System.out.println(f1.getAbsolutePath() + "       " + "//Copyright © 2023 Daniel Zhao");
+					}
 				}
 			}
+			}
+
 		}
-		
-		/*
-		 * Your task is to write a program that iterates through the src folder of this current Java Project. 
-		 * For every .java file it finds, the program will add a (non-legally binding) copyright statement at the bottom.
-		 * Be aware of possible directories inside of directories.
-		 * (e.g //Copyright © 2019 FirstName LastName)
-		 */
 	}
 }
